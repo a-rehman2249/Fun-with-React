873a30f4caaa83011463f3e19da2e08be7ee58a0
a-rehman2249 <a.rehman22049@gmail.com>
2026-07-01 16:59:32 +0500

    building linked post

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,62 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div style={{backgroundColor:'#ecf0f1',height:'100vh'}}>
+          <div style={{border:'2px solid black', display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',gap:'20px',paddingTop:'20px'}}>
+            <div>
+              <PostComponent/>
+            </div>
+            <div>
+              <PostComponent/>
+            </div>
+            <div>
+              <PostComponent/>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </>
   )
+}
+
+const my_style={
+  backgroundColor: 'white',
+  borderRadius: '10px',
+  borderWidth: '1px',
+  width:'300px',
+  padding:'20px',
+  borderColor:'grey',
+}
+
+function PostComponent(){
+    return(
+      <div style={my_style}>
+        <div style={{display:'flex'}}> 
+          <img src={'https://thumbs.dreamstime.com/b/portrait-handsome-smiling-young-man-folded-arms-smiling-joyful-cheerful-men-crossed-hands-isolated-studio-shot-172869765.jpg'}
+          style={{width:'40px',height:'40px',borderRadius: '50%', objectFit:'cover'}}
+          />
+          <div style={{fontSize:'10px',marginLeft:'5px'}}>
+            <div style={{fontWeight:'bold', fontSize:'12px'}}>
+              AbdulRehman
+            </div>
+            <div>
+              12000 followers
+            </div>
+            <div>
+              12m
+            </div>
+          </div>
+        </div>
+        <div style={{marginTop:'10px', fontSize:'14px'}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum possimus obcaecati labore incidunt quaerat.
+        </div>
+      </div>
+    )
 }
 
 export default App

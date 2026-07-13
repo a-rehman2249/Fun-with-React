92d814e46569c5ca89fee43601d89f675e68a90a
a-rehman2249 <a.rehman22049@gmail.com>
2026-07-13 12:11:21 -0700

    task 3 done

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <ToggleMessage/>
-      <ToggleMessage/>
-      <ToggleMessage/>
-    </>
-  )
-}
-
-
-const ToggleMessage=()=>{
-  const [visible, setVisible]= useState(true);
-
-  function toggleMessage(){
-    setVisible(!visible);
-  }
-
-  return(
-    <>
-      <div style={{border : '2px dotted black' , width: '400px'}}>
-        <div>
-          <button onClick={toggleMessage}>{visible ? 'Hide':'Show'}</button>
-        </div>
-        {visible && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem minima officiis expedita aliquid soluta? Voluptatum provident deserunt quibusdam reiciendis voluptate.</p>}
-        </div>
+      
     </>
   )
 }
